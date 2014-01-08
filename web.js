af318e9573313a55966378272a0a01aa4150b2ec
Cliff <cliffbarnes@live.com>
2014-01-08 09:35:07 -0500

    Install poet

--- web.js
+++ web.js
@@ -1,10 +1,50 @@
 // web.js
 var express = require("express");
 var app = express();
+var Poet = require('poet');
 
-app.get('/', function(req, res) {
-  res.send('Hello World!');
+// all environments
+app.use(express.static(__dirname + '/public'));
+app.use(express.cookieParser());
+app.use(express.session({ secret: 'keyboard cat' }));
+
+
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(express.cookieParser('arjuna'));
+
+// parses json, x-www-form-urlencoded, and multipart/form-data
+app.use(express.bodyParser());
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+//app routes
+
+var poet = Poet(app, {
+  postsPerPage: 3,
+  posts: './_posts',
+  metaFormat: 'json',
+  routes: {
+    '/myposts/:post': 'post',
+    '/pagination/:page': 'page',
+    '/mytags/:tag': 'tag',
+    '/mycategories/:category': 'category'
+  }
 });
+
+poet.init().then(function () {
+  // initialized
+});
+
+app.get('/', function (req, res) { res.render('index');});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
