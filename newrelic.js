dce48f6cab045dff2a9f05a2751b5201b4463eb2
Cliff <cliffbarnes@live.com>
2014-01-12 08:17:42 -0500

    add new relic

---
+++ newrelic.js
@@ -0,0 +1,24 @@
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name : ['cliffbarnes.com'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key : '88dd1c5d37b8e0c7f3db3239b4e572f66fec0e38',
+  logging : {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level : 'trace'
+  }
+};
